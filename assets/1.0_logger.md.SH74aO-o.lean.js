d56fa8badac00d6eb58e4cdd226b5f72bd91c262
marked <iczerwony98@gmail.com>
2023-12-03 21:27:21 +0100

    adding dist

---
+++ assets/1.0_logger.md.SH74aO-o.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,o as a,c as i,R as e}from"./chunks/framework.b72w6-dR.js";const c=JSON.parse('{"title":"Logger","description":"","frontmatter":{},"headers":[],"relativePath":"1.0/logger.md","filePath":"1.0/logger.md"}'),n={name:"1.0/logger.md"},t=e("",42),l=[t];function h(p,k,r,E,d,o){return a(),i("div",null,l)}const y=s(n,[["render",h]]);export{c as __pageData,y as default};
